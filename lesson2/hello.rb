2d441b72d9d8d37fad5dde35a4c7a83835eaad0f
andreyaksenov <andrey.aksenov@jetbrains.com>
2020-07-06 12:48:52 +0300

    Add RubyMine shortcuts

--- lesson2/hello.rb
+++ lesson2/hello.rb
@@ -1,2 +1,9 @@
 puts "Hello, funny world!"
 puts "Goodbye, cruel world..."
+
+# RubyMine shortcuts
+# Auto-indent line(s): ⌃ ⌥ I
+# Duplicate a current line or selected block: ⌘ D
+# Comment with line comment: ⌘ /
+# Move line (selection) up / down: ⌥ ⇧ ↑ / ⌥ ⇧ ↓
+# Delete the current line: ⌘⌫
